c16ac031449eabc67e03669e949e6d1af13a0cb9
ivanvgladkov <ivanvgladkov@gmail.com>
2016-08-20 16:51:37 +0300

    move binding to the register method

--- src/GeocodingServiceProvider.php
+++ src/GeocodingServiceProvider.php
@@ -4,23 +4,34 @@ namespace Ivanvgladkov\Geocoding;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Cache\Repository as CacheContract;
+use Illuminate\Contracts\Container\Container as ContainerContract;
 
+/**
+ * Class GeocodingServiceProvider
+ * @package Ivanvgladkov\Geocoding
+ */
 class GeocodingServiceProvider extends ServiceProvider
 {
     protected $defer = true;
 
     public function register()
     {
-        //
-    }
+        $this->app->bind(Geocoding::class, function (ContainerContract $app) {
+            $cache = $app->make(CacheContract::class);
+            $googleApiKey = $app->make('config')->get('geocoding.google_api_key');
 
-    public function boot(CacheContract $cache)
-    {
-        $this->app->bind(Geocoding::class, function ($app) use ($cache) {
-            $geocoding = new Geocoding($app['config']['app']['google']['api_key']);
+            $geocoding = new Geocoding($googleApiKey);
             $geocoding->setCache($cache);
+
             return $geocoding;
         });
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/geocoding.php' => config_path('geocoding.php')
+        ], 'config');
     }
 
     public function provides()
